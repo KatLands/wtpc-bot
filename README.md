bcd3f6bf7aeb8d42a6946ddd27b80c70bb4a4a5f
KatLands <dev.kat.lands@gmail.com>
2021-10-13 17:31:37 -0400

    Updated README and deploy commands
    Updated README and deploy commands

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# wtpc-bot-js
+## Wake Tech Programming Club Bot
+
+-Manages server for the club with scheduled reminder messages for meetings. 
+-Ability to get coding questions of varying difficulty via binarysearch.
+
+
+
+## Commands
+
+- `/ping`: Confirms bot is up and running
+- `/server`: Responds with current server member count
+- `/question easy`: Returns question from binary search with easy difficulty
+- `/question medium`: Returns question from binary search with medium difficulty
+- `/question hard`: Returns question from binary search with hard difficulty
+- `/question veryhard`: Returns question from binary search with very hard difficulty
+
--- bot.js
+++ bot.js
@@ -1,70 +1,75 @@
-const { Client, Intents, MessageEmbed } = require("discord.js");
-const { token, targetChl, guildId } = require("./config.json");
-const CronJob = require("cron").CronJob;
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const { token, targetChl, guildId } = require('./config.json');
+const CronJob = require('cron').CronJob;
 
-const getRandomQuestion = require("./utils.js");
+const getRandomQuestion = require('./utils.js');
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS],
+	intents: [Intents.FLAGS.GUILDS],
 });
 
-client.on("ready", () => {
-  console.log(`Logged in: ${client.user.tag}`);
+client.on('ready', () => {
+	console.log(`Logged in: ${client.user.tag}`);
 });
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
-  const guild = client.guilds.cache.get(guildId);
-  const memberCount = guild.memberCount;
+	const { commandName } = interaction;
+	const guild = client.guilds.cache.get(guildId);
+	const memberCount = guild.memberCount;
 
-  if (commandName == "ping") {
-    await interaction.reply("I'm alive!");
-  } else if (commandName == "question") {
-    if (interaction.options.getSubcommand() === "easy") {
-      const embeddedQuestion = await getRandomQuestion(0);
-      await interaction.channel.send({ embeds: [embeddedQuestion] });
-    } else if (interaction.options.getSubcommand() === "medium") {
-      const embeddedQuestion = await getRandomQuestion(1);
-      await interaction.channel.send({ embeds: [embeddedQuestion] });
-    } else if (interaction.options.getSubcommand() === "hard") {
-      const embeddedQuestion = await getRandomQuestion(2);
-      await interaction.channel.send({ embeds: [embeddedQuestion] });
-    } else if (interaction.options.getSubcommand() === "veryhard") {
-      const embeddedQuestion = await getRandomQuestion(3);
-      await interaction.channel.send({ embeds: [embeddedQuestion] });
-    }
-  } else if (commandName == "server") {
-    await interaction.reply(`WTPC Current Member Count: ${memberCount}`);
-  }
+	if (commandName == 'ping') {
+		await interaction.reply('I\'m alive!');
+	}
+	else if (commandName == 'question') {
+		if (interaction.options.getSubcommand() === 'easy') {
+			const embeddedQuestion = await getRandomQuestion(0);
+			await interaction.channel.send({ embeds: [embeddedQuestion] });
+		}
+		else if (interaction.options.getSubcommand() === 'medium') {
+			const embeddedQuestion = await getRandomQuestion(1);
+			await interaction.channel.send({ embeds: [embeddedQuestion] });
+		}
+		else if (interaction.options.getSubcommand() === 'hard') {
+			const embeddedQuestion = await getRandomQuestion(2);
+			await interaction.channel.send({ embeds: [embeddedQuestion] });
+		}
+		else if (interaction.options.getSubcommand() === 'veryhard') {
+			const embeddedQuestion = await getRandomQuestion(3);
+			await interaction.channel.send({ embeds: [embeddedQuestion] });
+		}
+	}
+	else if (commandName == 'server') {
+		await interaction.reply(`WTPC Current Member Count: ${memberCount}`);
+	}
 });
 
 // '* * * * * *'
 // sec(0-59), min(0-59), hour(0-23), day of month(1-31), month(1-12), day of week(0-6 starting with sunday)
-const dayBeforeReminder = new CronJob("1 12 * * 4", function () {
-  const dayBeforeMsg = new MessageEmbed()
-    .setColor("#0080ff")
-    .addFields({
-      name: "Meeting this Friday at 7pm",
-      value: "React with 👍 to RSVP",
-    })
-    .setImage(
-      "https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png"
-    );
+const dayBeforeReminder = new CronJob('1 12 * * 4', function() {
+	const dayBeforeMsg = new MessageEmbed()
+		.setColor('#0080ff')
+		.addFields({
+			name: 'Meeting this Friday at 7pm',
+			value: 'React with 👍 to RSVP',
+		})
+		.setImage(
+			'https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png',
+		);
 
-  client.channels.cache.get(targetChl).send({ embeds: [dayBeforeMsg] });
+	client.channels.cache.get(targetChl).send({ embeds: [dayBeforeMsg] });
 });
 
-const meetingStart = new CronJob("58 18 * * 5", function () {
-  const mtgStartMsg = new MessageEmbed()
-    .setColor("#0080ff")
-    .addFields({ name: "Meeting starting now", value: "Join general chat" })
-    .setImage(
-      "https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png"
-    );
+const meetingStart = new CronJob('58 18 * * 5', function() {
+	const mtgStartMsg = new MessageEmbed()
+		.setColor('#0080ff')
+		.addFields({ name: 'Meeting starting now', value: 'Join general chat' })
+		.setImage(
+			'https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png',
+		);
 
-  client.channels.cache.get(targetChl).send({ embeds: [mtgStartMsg] });
+	client.channels.cache.get(targetChl).send({ embeds: [mtgStartMsg] });
 });
 
 dayBeforeReminder.start();
