9ff5d144fd3c8bd48b00b97922cacd9eb4538a2a
KatLands <dev.kat.lands@gmail.com>
2021-10-13 15:21:00 -0400

    ESLint
    Added ESLint with standard discord.js rules.
    
    Added bot response for command  /server.

--- bot.js
+++ bot.js
@@ -1,58 +1,62 @@
-const {Client, Intents, MessageEmbed} = require('discord.js');
-const {token} = require('./config.json');
-const {targetChl} = require('./config.json')
-var CronJob = require('cron').CronJob;
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const { token, targetChl, guildId } = require('./config.json');
+const CronJob = require('cron').CronJob;
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS],
+	intents: [Intents.FLAGS.GUILDS],
 });
 
 
 client.on('ready', () => {
-    console.log(`Logged in: ${client.user.tag}`)
+	console.log(`Logged in: ${client.user.tag}`);
 });
 
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+	if (!interaction.isCommand()) return;
 
-    const {commandName} = interaction;
+	const { commandName } = interaction;
+	const guild = client.guilds.cache.get(guildId);
+	const memberCount = guild.memberCount;
 
-    if (commandName == 'ping') {
-        await interaction.reply("I'm alive!");
-    }
+	if (commandName == 'ping') {
+		await interaction.reply('I\'m alive!');
+	}
+	else if (commandName == 'server') {
+		await interaction.reply(`WTPC Current Member Count: ${memberCount}`);
+	}
 });
 
 
 // '* * * * * *'
-// sec(0-59), min(0-59), hour(0-23), day of month(1-31), month(1-12), day of week(0-6 starting with sunday)  
+// sec(0-59), min(0-59), hour(0-23), day of month(1-31), month(1-12), day of week(0-6 starting with sunday)
 const dayBeforeReminder = new CronJob('1 12 * * 4', function() {
-    const dayBeforeMsg = new MessageEmbed()
-        .setColor('#0080ff')
-        .addFields(
-            {name: 'Meeting this Friday at 7pm',
-            value: 'React with 👍 to RSVP'}
-        )
-        .setImage('https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png')
+	const dayBeforeMsg = new MessageEmbed()
+		.setColor('#0080ff')
+		.addFields(
+			{ name: 'Meeting this Friday at 7pm',
+				value: 'React with 👍 to RSVP' },
+		)
+		.setImage('https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png');
 
-    client.channels.cache.get(targetChl).send({embeds: [dayBeforeMsg]});   
+	client.channels.cache.get(targetChl).send({ embeds: [dayBeforeMsg] });
 });
 
 
 const meetingStart = new CronJob('58 18 * * 5', function() {
-    const mtgStartMsg = new MessageEmbed()
-        .setColor('#0080ff')
-        .addFields(
-            {name: 'Meeting starting now',
-            value: 'Join general chat'}
-        )
-        .setImage('https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png')
-        
-    client.channels.cache.get(targetChl).send({embeds: [mtgStartMsg]});   
+	const mtgStartMsg = new MessageEmbed()
+		.setColor('#0080ff')
+		.addFields(
+			{ name: 'Meeting starting now',
+				value: 'Join general chat' },
+		)
+		.setImage('https://www.waketech.edu/themes/custom/talon/assets/images/wake-tech-2017.png');
+
+	client.channels.cache.get(targetChl).send({ embeds: [mtgStartMsg] });
 });
 
 
 dayBeforeReminder.start();
-meetingStart.start()
+meetingStart.start();
 
 client.login(token);
