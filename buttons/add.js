a58bd5d9e85138d86b0790462d3e47e626cdfe81
Ali Ahmad <alislearning123@gmail.com>
2021-11-13 11:39:06 -0500

    Change RSVP Member Bullets From Dash (-) To Square Emoji

--- buttons/add.js
+++ buttons/add.js
@@ -6,7 +6,7 @@ module.exports = {
 
         const originalDescription = message.embeds[0].description.split('** **')[0] + '** **';
 
-        const RSVPArray = message.embeds[0].description.split('- ').splice(1).map(item => item.trim());
+        const RSVPArray = message.embeds[0].description.split(':white_small_square: ').splice(1).map(item => item.trim());
 
         if (!RSVPArray.includes(member.displayName)) {
             RSVPArray.push(member.displayName);
@@ -14,7 +14,7 @@ module.exports = {
 
         message.embeds[0].description = originalDescription;
         message.embeds[0].description += `\n\n**Current attendees: ${RSVPArray.length}**\n`;
-        message.embeds[0].description += RSVPArray.length ? ' - ' + RSVPArray.join('\n - ') : ':smiling_face_with_tear:';
+        message.embeds[0].description += RSVPArray.length ? ' :white_small_square: ' + RSVPArray.join('\n :white_small_square: ') : ':smiling_face_with_tear:';
         message.edit({ embeds: [message.embeds[0]], components: [message.components[0]] });
 
         return interaction.deferUpdate();
--- buttons/remove.js
+++ buttons/remove.js
@@ -6,11 +6,11 @@ module.exports = {
 
         const originalDescription = message.embeds[0].description.split('** **')[0] + '** **';
 
-        const RSVPArray = message.embeds[0].description.split('- ').splice(1).map(item => item.trim()).filter(item => item !== member.displayName);
+        const RSVPArray = message.embeds[0].description.split(':white_small_square: ').splice(1).map(item => item.trim()).filter(item => item !== member.displayName);
 
         message.embeds[0].description = originalDescription;
         message.embeds[0].description += `\n\n**Current attendees: ${RSVPArray.length}**\n`;
-        message.embeds[0].description += RSVPArray.length ? ' - ' + RSVPArray.join('\n - ') : ':smiling_face_with_tear:';
+        message.embeds[0].description += RSVPArray.length ? ' :white_small_square: ' + RSVPArray.join('\n :white_small_square: ') : ':smiling_face_with_tear:';
         message.edit({ embeds: [message.embeds[0]], components: [message.components[0]] });
 
         return interaction.deferUpdate();
